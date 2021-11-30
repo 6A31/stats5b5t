281dc67746dc15a9278df29492e26fa97b098678
Scobra <79657875+ScobraScope@users.noreply.github.com>
2021-11-30 16:10:06 +0100

    Some setup.py changes

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
+import pathlib
 from setuptools import setup, find_packages
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
-VERSION = '0.1.0' 
+VERSION = '0.1.3' 
 DESCRIPTION = 'API Wrapper for 5b5t.org'
-LONG_DESCRIPTION = 'Automate and simplify API requests for 5b5t.org \nA detailed documentation can be found at https://github.com/ScobraScope/stats5b5t-documentation'
+
 
 setup(
        # the name must match the folder name 'verysimplemodule'
         name="stats5b5t", 
         version=VERSION,
         author="ScobraScope",
-        link="https://github.com/ScobraScope/stats5b5t",
+        url="https://github.com/ScobraScope/stats5b5t",
         description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
+        long_description=README,
         packages=find_packages(),
-        install_requires=[
+        long_description_content_type='text/markdown',
+        install_requires=[  
             "requests"
         ],
-        keywords=['python', 'api', ''],
+        keywords=['python', 'api', '5b5t', 'minecraft'],
         classifiers= [
             "Operating System :: Microsoft :: Windows",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 3.9",
+            "Topic :: Games/Entertainment", 
         ]
 )
