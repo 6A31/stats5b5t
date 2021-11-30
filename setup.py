1a08b75f2d71246d79e69c286f878e77efa70be3
Scobra <79657875+ScobraScope@users.noreply.github.com>
2021-11-30 13:38:49 +0100

    Adding a setup.py file, should be the last change before uploading to pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'API Wrapper for 5b5t.org'
+LONG_DESCRIPTION = 'Automate and simplify api requests  for 5b5t.org'
+
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="stats5b5t", 
+        version=VERSION,
+        author="ScobraScope",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+            "requests"
+        ],
+        keywords=['python', 'api', ''],
+        classifiers= [
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
