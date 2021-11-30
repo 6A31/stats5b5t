b199c73df1fd46bf4896f92598576b4b49390f8a
Scobra <79657875+ScobraScope@users.noreply.github.com>
2021-11-30 14:20:28 +0100

    Final change

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 VERSION = '0.1.0' 
 DESCRIPTION = 'API Wrapper for 5b5t.org'
-LONG_DESCRIPTION = 'Automate and simplify api requests  for 5b5t.org'
+LONG_DESCRIPTION = 'Automate and simplify API requests for 5b5t.org \nA detailed documentation can be found at https://github.com/ScobraScope/stats5b5t-documentation'
 
 setup(
        # the name must match the folder name 'verysimplemodule'
         name="stats5b5t", 
         version=VERSION,
         author="ScobraScope",
+        link="https://github.com/ScobraScope/stats5b5t",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
